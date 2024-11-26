6d12862fe5a34d6a00010f3683815190067feaf1
Adrian du Plessis <8139635+AdrianDUP@users.noreply.github.com>
2024-11-26 21:26:44 +0100

    Implemented the boilerplate for the advent of code

---
+++ 2024/go/pkg/solver/day1.go
@@ -0,0 +1,12 @@
+package solver
+
+type SolverOne struct {
+}
+
+func (solver SolverOne) SolutionOne(lines []string) int {
+    return 0;
+}
+
+func (solver SolverOne) SolutionTwo(lines []string) int {
+    return 1;
+}
---
+++ 2024/go/pkg/solver/day7.go
@@ -0,0 +1,12 @@
+package solver
+
+type SolverSeven struct {
+}
+
+func (solver SolverSeven) SolutionOne(lines []string) int {
+    return 0;
+}
+
+func (solver SolverSeven) SolutionTwo(lines []string) int {
+    return 1;
+}
