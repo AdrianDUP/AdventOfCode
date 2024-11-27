35f2e4794de3966ded0abc9f426d02e2ea4e9666
Adrian du Plessis <adrian.du.plessis@basworld.com>
2024-11-27 12:48:00 +0100

    Copied over solution of day 1 part 1 2023

--- go/pkg/2023/day1.go
+++ go/pkg/2023/day1.go
@@ -1,10 +1,40 @@
 package solver2023
 
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+)
+
 type SolverOne struct {
 }
 
 func (solver SolverOne) SolutionOne(lines []string) int {
-    return 0;
+    answer := 0
+    regExp, _ := regexp.Compile("[0-9]")
+
+    for _, element := range lines {
+        if element == "" {
+            continue
+        }
+
+        numbers := regExp.FindAllString(element, -1)
+
+        if len(numbers) == 0 {
+            panic("No numbers found")
+        }
+
+        number := fmt.Sprintf("%s%s", numbers[0], numbers[len(numbers)-1])
+        convertedNumber, err := strconv.Atoi(number)
+
+        if err != nil {
+            panic("Could not make number")
+        }
+
+        answer += convertedNumber
+    }
+
+    return answer;
 }
 
 func (solver SolverOne) SolutionTwo(lines []string) int {
