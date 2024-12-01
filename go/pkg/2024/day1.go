f08dd866449a1c7ddf6e73171c3e15e553ff5ead
Adrian du Plessis <8139635+AdrianDUP@users.noreply.github.com>
2024-12-01 09:09:13 +0100

    Added solution for 2024 day 1

--- go/pkg/2024/day1.go
+++ go/pkg/2024/day1.go
@@ -1,12 +1,87 @@
 package solver2024
 
+import (
+	"fmt"
+	"slices"
+	"strconv"
+	"strings"
+)
+
 type SolverOne struct {
 }
 
 func (solver SolverOne) SolutionOne(lines []string) int {
-    return 0;
+    answer := 0
+
+    leftSide := []int{}
+    rightSide := []int{}
+
+    for _, element := range lines {
+        numbers := strings.Split(element, " ")
+        leftNumber, err := strconv.Atoi(numbers[0])
+        if  err != nil {
+            panic(fmt.Sprintf("Number %s is not valid", numbers[0]))
+        }
+        rightNumber, err := strconv.Atoi(numbers[len(numbers)-1])
+        if  err != nil {
+            panic(fmt.Sprintf("Number %s is not valid", numbers[len(numbers)-1]))
+        }
+
+        leftSide = append(leftSide, leftNumber)
+        rightSide = append(rightSide, rightNumber)
+    }
+
+    slices.Sort(leftSide)
+    slices.Sort(rightSide)
+
+    for index, element := range leftSide {
+        distance := element - rightSide[index]
+        if (distance < 0) {
+            distance = distance * -1
+        }
+
+        answer += distance
+    }
+
+    return answer;
 }
 
 func (solver SolverOne) SolutionTwo(lines []string) int {
-    return 1;
+    answer := 0
+
+    leftSide := []int{}
+    rightSide := []int{}
+
+    for _, element := range lines {
+        numbers := strings.Split(element, " ")
+        leftNumber, err := strconv.Atoi(numbers[0])
+        if  err != nil {
+            panic(fmt.Sprintf("Number %s is not valid", numbers[0]))
+        }
+        rightNumber, err := strconv.Atoi(numbers[len(numbers)-1])
+        if  err != nil {
+            panic(fmt.Sprintf("Number %s is not valid", numbers[len(numbers)-1]))
+        }
+
+        leftSide = append(leftSide, leftNumber)
+        rightSide = append(rightSide, rightNumber)
+    }
+
+    counts := make(map[int]int)
+
+    for _, element := range rightSide {
+        if val, ok := counts[element]; ok {
+            counts[element] = val + 1;
+        } else {
+            counts[element] = 1
+        }
+    }
+
+    for _, element := range leftSide {
+        if val, ok := counts[element]; ok {
+            answer += val * element
+        } 
+    }
+
+    return answer
 }
