64de0b8de6a393e6dfcfbdf2a57e3c86266e10e3
Adrian du Plessis <8139635+AdrianDUP@users.noreply.github.com>
2024-12-06 09:05:37 +0100

    started day 6

--- rust/aoc/src/solver/s24/day6.rs
+++ rust/aoc/src/solver/s24/day6.rs
@@ -1,14 +1,42 @@
-use crate::solver::solver::Solver;
+use std::collections::HashMap;
+
+use crate::solver::solver::{split_string_into_characters, Solver};
 
 pub struct Day6 {
 }
 
 impl Solver for Day6 {
     fn solution_one(&self, lines: Vec<String>) -> i64 {
+        let mut answer = 0;
+
+        let grid = get_grid(lines);
+
+        
+
         return 0;
     }
 
     fn solution_two(&self, lines: Vec<String>) -> i64 {
         return 0;
+    }
+}
+
+fn get_grid(lines: Vec<String>) -> Vec<Vec<String>> {
+    let mut grid: Vec<Vec<String>> = vec![];
+
+    for line in lines {
+        grid.push(split_string_into_characters(line));
+    }
+
+    return grid;
+}
+
+fn find_start(grid: Vec<Vec<String>>) -> (i64, i64) {
+    for (y, element) in grid.iter().enumerate() {
+        for (x, element2) in element.iter().enumerate() {
+            if element == "^" {
+                return (x,y);
+            }
+        }
     }
 }
